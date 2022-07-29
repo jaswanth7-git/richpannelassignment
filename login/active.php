b00a4cf195a6a94ed7011158d9bd10c7e8f2cae1
jaswanth7-git <jaswanthchowdary0707@gmail.com>
2022-07-29 12:07:31 +0530

    last

--- login/active.php
+++ login/active.php
@@ -5,10 +5,17 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/price.css">
     <title>Document</title>
-<div class="col-lg-3 col-md-6 col-12">
+    <nav>
+    <ul>
+        <li><a href="./index.php">Jaswanth Alla</a></li>
+        <li><a href="./active.php">Active Plans</a></li>
+    </ul>
+    </nav>
+  
+<div class="col-lg-3 col-md-6 col-12 mt-4">
             <div class="price-card">
-                <p class="plan">Standard<p>
-                    <p ><span class="price">Rs49.99</span>/month</p>
+                <p class="plan">premium<p>
+                    <p ><span class="price">$99.99</span>/month</p>
                     <div class="features">
                     <p >Better Video Quality</p>
                     <p>1080p Resolution</p>
@@ -17,3 +24,4 @@
                    <a href="./refund.html"><button class="button-66" role="button">Unsubscribe</button></a> 
             </div>
         </div>
+     
